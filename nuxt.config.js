3345fe4ab56326ec5fbba7c012093fc51bffd982
MahardiikaKD <dikamahar884@gmail.com>
2023-03-27 00:00:31 +0700

    web image logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/bimba.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/smile-header.png' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap',
